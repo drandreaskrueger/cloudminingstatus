e4e9e7448fde3fbeb022cba7186f63f156e69416
drandreaskrueger <study@andreaskrueger.de>
2017-02-12 20:28:27 +0000

    windows starter - see sourcecode for window layout hint

---
+++ start.bat
@@ -0,0 +1,11 @@
+REM you can choose windowsize, colors, font, etc
+REM by running this, then clicking on 
+REM TopLeftCorner icon of cmd.exe window 
+REM    Properties
+REM       Font
+REM       Layout
+REM       Colors
+REM it should remember it for next time.
+
+python cloudminingstatus.py
+pause
